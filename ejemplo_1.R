4a16d473555cb62d926b6ab0f0e172ec36d593d7
gabodeoz <35463119+gabodeoz@users.noreply.github.com>
2023-05-12 16:16:09 -0600

    Update ejemplo_1.R

--- ejemplo_1.R
+++ ejemplo_1.R
@@ -1,3 +1,5 @@
+# Extraido de https://www.statology.org/lm-function-in-r/
+
 df = data.frame(x=c(1, 3, 3, 4, 5, 5, 6, 8, 9, 12),y=c(12, 14, 14, 13, 17, 19, 22, 26, 24, 22))
 model <- lm(y ~ x, data=df)
 summary(model)
