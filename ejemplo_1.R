f1abac37befb936625a79defab6299a566fec55c
gabodeoz <35463119+gabodeoz@users.noreply.github.com>
2023-05-12 16:15:13 -0600

    Create ejemplo_1.R

---
+++ ejemplo_1.R
@@ -0,0 +1,5 @@
+df = data.frame(x=c(1, 3, 3, 4, 5, 5, 6, 8, 9, 12),y=c(12, 14, 14, 13, 17, 19, 22, 26, 24, 22))
+model <- lm(y ~ x, data=df)
+summary(model)
+lm(formula = y ~ x, data = df)
+plot(model)
